4c297e078f13318b081d1f536913eb670a5b8741
bomberman-sy <kenfackkeng1995@gmail.com>
2022-05-05 13:45:07 +0200

    initial

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import About from './components/About';
+import Home from './components/Home';
+
+function App() {
+  return (
+  <>
+  <h1>home home</h1>
+  </>
+   
+  );
+}
+
+export default App;
---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+//import { Children } from "react";
+
+
+export default function Home() {
+   
+  return (
+    <div>
+    <h1> HOME </h1>
+    <br/>
+    <p> lo </p>
+    </div>
+  )
+}
