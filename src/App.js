3a69fa29f7c78b38f8cfb3edd0aaa344f70c6aa9
bomberman-sy <kenfackkeng1995@gmail.com>
2022-05-05 14:54:21 +0200

    changes for v2

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 
 import './App.css';
-import About from './components/About';
-import Home from './components/Home';
+//gitimport About from './components/About';
+//import Home from './components/Home';
 
 function App() {
   return (
   <>
   <h1>home home</h1>
+  <ul>
+  <li><a href="">lundi</a></li>
+  <li><a href="">mardi</a></li>
+ <li><a href="">mercredi</a></li>
+  </ul>
   </>
    
   );
